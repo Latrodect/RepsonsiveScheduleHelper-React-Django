cc14d2ad8cabf3312a4098d43741ac2a9964a830
Latrodect <h.bahadirnural@gmail.com>
2023-07-13 21:16:01 +0300

    Base mock development of Favorites Dropdown

--- mock/api/__init__.py
+++ mock/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api.endpoints import notifications
+from api.endpoints import favorites
 
 app = FastAPI()
 
 app.include_router(notifications.router, prefix="/api/v1")
+app.include_router(favorites.router, prefix="/api/v1")

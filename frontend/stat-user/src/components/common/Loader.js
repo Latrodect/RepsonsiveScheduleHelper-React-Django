98f060352b72931b0aec1e53d8fc356a17f4c02a
Latrodect <h.bahadirnural@gmail.com>
2023-08-01 17:43:40 +0300

    Loader completa

--- frontend/stat-user/src/components/common/Loader.js
+++ frontend/stat-user/src/components/common/Loader.js
@@ -3,10 +3,15 @@ import React, { Suspense } from "react";
 export default function Loader(Component) {
   function Loading(params) {
     return (
-      <Suspense fallback={<div> ...loading</div>}>
+      <Suspense fallback={
+      <div class="ring">Wasp
+      <span class="ringSpawn"></span>
+    </div>
+  }>
         <Component {...params} />
       </Suspense>
     );
   }
   return Loading;
 }
+
